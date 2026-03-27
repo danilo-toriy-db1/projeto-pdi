fb6c0fd0d490adb1178c0444946e595447d35c09
Danilo Riki Toriy <danilo.toriy@db1.com.br>
2026-03-27 11:05:42 -0300

    bugfix: arrumado bug de alternância de modos caso trocasse de página - erro lógico no JS, onde um alternava a classe do body e outro do html

--- projeto-landing-page/js/script.js
+++ projeto-landing-page/js/script.js
@@ -1,9 +1,9 @@
 function changeMode(){
     if(localStorage.getItem("tema") === "light"){
-        document.body.classList.add("dark-mode");
+        document.documentElement.classList.add("dark-mode");
         localStorage.setItem("tema", "dark");
     }else{
-        document.body.classList.remove("dark-mode");
+        document.documentElement.classList.remove("dark-mode");
         localStorage.setItem("tema", "light");
     }
 }

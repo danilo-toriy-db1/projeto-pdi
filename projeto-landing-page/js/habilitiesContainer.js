80efe9d3a7d1f29f9bed51777fd8ef677cc8f92d
Danilo Riki Toriy <danilo.toriy@db1.com.br>
2026-04-01 14:54:39 -0300

    feat: funcionalidade de listagem de habilidades via JavaSCript criada. Visual incompleto

---
+++ projeto-landing-page/js/habilitiesContainer.js
@@ -0,0 +1,20 @@
+const habilidades = [
+    {"habilidade": "HTML",
+     "tipo": "Hard Skill"   
+    },
+    {"habilidade": "CSS",
+     "tipo": "Hard Skill"   
+    },
+    {"habilidade": "JavaScript",
+     "tipo": "Hard Skill"   
+    },
+    {"habilidade": "Comunicação",
+     "tipo": "Soft Skill"   
+    },
+    {"habilidade": "Trabalho em equipe",
+    "tipo": "Soft Skill"}
+]
+
+export function getHabilities(){
+    return habilidades;
+}
---
+++ projeto-landing-page/js/habilitiesLoader.js
@@ -0,0 +1,39 @@
+import { getHabilities } from "./habilitiesContainer.js";  
+
+function loadHabilities(){
+    const habilities = getHabilities();
+    const habilitiesContainer = document.getElementById("habilidades__cards--container");
+
+    habilitiesContainer.innerHTML = '';
+
+    if (habilities.length === 0) {
+        const div = document.createElement('div');
+        div.className = 'vazio'
+
+        div.innerHTML = `
+                    <h3>Não há habilidades listadas...</h3>
+        `
+
+        habilitiesContainer.appendChild(div);
+        return;
+    }
+
+    habilities.forEach(habilidade => {
+        const div = document.createElement('div');
+
+        const tipo = habilidade.tipo === 'Hard Skill'
+                ? 'hard-skill'
+                : 'soft-skill'
+        
+        div.className = `habilidade__type ${tipo}`
+        
+        div.innerHTML = `
+                <div class="habilidade__content">
+                    <h3> ${habilidade.habilidade} </h3>
+                </div>
+        `
+        habilitiesContainer.appendChild(div);
+    });
+}
+
+loadHabilities();

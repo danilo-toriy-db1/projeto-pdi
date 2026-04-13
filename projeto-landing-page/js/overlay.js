6766849276487b475edeb54b7e2b7c57557d36da
Danilo Riki Toriy <danilo.toriy@db1.com.br>
2026-04-13 15:51:30 -0300

    feat: criado a função de adicionar uma nova habilidade, usando overlay para definição de escolhas, eventos de submit e click

--- projeto-landing-page/js/overlay.js
+++ projeto-landing-page/js/overlay.js
@@ -3,48 +3,64 @@ import { addHabilities } from "./habilitiesContainer.js"
 const btnAbrirPopUp = document.getElementById("button__openPopUp");
 
 btnAbrirPopUp.addEventListener('click', () => {
-    const div = document.createElement('div');
+    const overlay = document.createElement('div');
 
-    div.id = 'overlay__container';
+    overlay.className = 'overlay__display';
 
-    div.innerHTML = `
-        <form id="form__container">
-            <div class="input__container">
-                <label for="nova-habilidade">Habilidade: </label>
-                <input type="text" name="nova-habilidade" id="nova-habilidade" placeholder="Digite a habilidade aqui..." required>
-                <label for="tipo-habilidade" id="tipo">Tipo: </label>
-                <select name="tipo" id="tipo-habilidade" required>
-                    <option value="soft">Soft-Skill</option>
-                    <option value="hard">Hard-Skill</option>
-                </select>
-            </div>
-            <div class="button__container">
-                <button type="reset" id="button__reset">Limpar</button>
-                <button id="button__submit">Enviar</button>
-            </div>
-        </form>
+    overlay.innerHTML = `
+        <div class="overlay__container">
+            <div class="close__container">
+                <button id="button__close">
+                    <img src="/projeto-landing-page/assets/icons/close-icon.svg" alt="Ícone do botão de fechar">
+                </button>
+            </div> 
+            <form id="form__container">
+                <div class="input__container">
+                    <label for="nova-habilidade">Habilidade: </label>
+                    <input type="text" name="nova-habilidade" id="nova-habilidade" placeholder="Digite a habilidade aqui..." required>
+                    <label for="tipo-habilidade" id="tipo">Tipo: </label>
+                    <select name="tipo" id="tipo-habilidade" required>
+                        <option value="soft">Soft-Skill</option>
+                        <option value="hard">Hard-Skill</option>
+                    </select>
+                </div>
+                <div class="button__container">
+                    <button type="reset" id="button__reset">Limpar</button>
+                    <button id="button__submit">Enviar</button>
+                </div>
+            </form>
+        </div>
     `;
 
-    document.body.appendChild(div);
+    document.body.appendChild(overlay);
+
+    const btnFecharPopUp = overlay.querySelector('#button__close');
+
+    btnFecharPopUp.addEventListener('click', () => {
+        overlay.remove();
+    });
+
+    const formulario = document.getElementById("form__container");
+
+    formulario.addEventListener("submit", (evento) => {
+
+        evento.preventDefault();
+
+        let habilidade = document.getElementById("nova-habilidade").value;
+        let tipo = document.getElementById("tipo-habilidade").value === "soft"
+                ? "Soft Skill"
+                : "Hard Skill";
+
+        const novaHabilidade = {
+            "habilidade": habilidade,
+            "tipo": tipo
+        };
+
+        addHabilities(novaHabilidade);
+
+        formulario.reset();
+        overlay.remove();
+    });
+
 });
 
-const formulario = document.getElementById("form__container");
-
-formulario.addEventListener("submit", (evento) => {
-
-    evento.preventDefault();
-
-    let habilidade = document.getElementById("nova-habilidade").value;
-    let tipo = document.getElementById("tipo-habilidade").value === "soft"
-            ? "Soft Skill"
-            : "Hard Skill";
-
-    const novaHabilidade = {
-        "habilidade": habilidade,
-        "tipo": tipo
-    };
-
-    addHabilities(novaHabilidade);
-
-    formulario.reset();
-})
